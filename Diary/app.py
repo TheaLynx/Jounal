cb9620c859d88b6cbf6400e65041168df870d4e8
Sarah Pantzer <sarah@pantzer.me>
2025-03-05 10:29:57 +0100

    App placed

---
+++ Diary/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request
+import datetime
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return open("index.html").read()  # Stellt sicher, dass dein HTML-File im gleichen Verzeichnis liegt
+
+@app.route("/save", methods=["POST"])
+def save():
+    eintrag = request.form["eintrag"]
+    datum = datetime.date.today().strftime("%Y-%m-%d")
+
+    filename = f"tagebuch_{datum}.txt"
+    
+    with open(filename, "a", encoding="utf-8") as file:
+        file.write(f"{datum}:\n{eintrag}\n\n")
+
+    return "Eintrag gespeichert! <a href='/'>Zurück</a>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
